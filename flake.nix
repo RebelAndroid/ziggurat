f5aa79f0899c38756323fda36d8a238feb235a0e
Christopher McDevitt <47703105+RebelAndroid@users.noreply.github.com>
2024-05-24 12:05:29 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+        let pkgs = import nixpkgs { inherit system; }; in
+        with pkgs; {
+          devShells.default = mkShell {
+            buildInputs = [
+              zig
+              zls
+              xorriso
+              qemu
+              parted
+            ];
+          };
+        }
+    );
+}
